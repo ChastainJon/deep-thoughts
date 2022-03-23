f042d795732f80a19177efc0bcc829e504e9a37e
ChastainJon <chastain.r.jon@gmail.com>
2022-03-23 13:14:35 -0400

    add token authorization

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
+import Auth from '../utils/auth';
 
 const Login = (props) => {
   const [formState, setFormState] = useState({ email: '', password: '' });
-
+  const [login, {error}] = useMutation(LOGIN_USER)
   // update state based on form input changes
   const handleChange = (event) => {
     const { name, value } = event.target;
 
+    
     setFormState({
       ...formState,
       [name]: value,
@@ -17,6 +21,13 @@ const Login = (props) => {
   const handleFormSubmit = async (event) => {
     event.preventDefault();
 
+    try{
+      const {data} = await login({ variables: {...formState}})
+      
+      Auth.login(data.login.token)
+    } catch (e){
+      console.error(e)
+    }
     // clear form values
     setFormState({
       email: '',
@@ -53,6 +64,7 @@ const Login = (props) => {
                 Submit
               </button>
             </form>
+            {error && <div>Login Failed</div>}
           </div>
         </div>
       </div>
