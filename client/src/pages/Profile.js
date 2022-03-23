ca59b6c4ab45e5c61f45167d90e35ea0dd54086e
ChastainJon <chastain.r.jon@gmail.com>
2022-03-23 13:52:50 -0400

    add friends list, implement user based queries

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,17 +1,23 @@
 import React from 'react';
-import { useParams } from 'react-router-dom'
-import ThoughtList from '../components/ThoughtList';
+import { Redirect, useParams } from 'react-router-dom'
 import { useQuery } from '@apollo/client';
-import { QUERY_USER } from '../utils/queries';
+import { QUERY_USER, QUERY_ME } from '../utils/queries';
+import Auth from '../utils/auth';
+
+import ThoughtList from '../components/ThoughtList';
 import FriendList from '../components/FriendList';
 
 const Profile = () => {
 
-  const { username: useParam } = useParams()
-  const { loading, data } = useQuery(QUERY_USER, {
-    variables: {username: useParam}
+  const { username: userParam } = useParams()
+  const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
+    variables: {username: userParam}
   })
-  const user = data?.user || {}
+  const user = data?.me || data?.user || {}
+
+  if(Auth.loggedIn() && Auth.getProfile().data.username === userParam){
+    return <Redirect to="/profile" />
+  }
 
   if(loading){
     return <div>Loading...</div>
@@ -21,15 +27,25 @@ const Profile = () => {
     <div>
       <div className="flex-row mb-3">
         <h2 className="bg-dark text-secondary p-3 display-inline-block">
-            Viewing {user.username}'s profile.
+          Viewing {userParam ? `${user.username}'s` : 'your'} profile.
         </h2>
       </div>
 
       <div className="flex-row justify-space-between mb-3">
-        <ThoughtList thoughts={user.thoughts} title={`${user.username}'s thoughts...`}/>
-      </div>
-      <div className="flex-row justify-space-between mb-3">
-        <FriendList username={user.username} friendCount={user.friendCount} friends={user.friends}></FriendList>
+        <div className="col-12 mb-3 col-lg-8">
+          <ThoughtList
+            thoughts={user.thoughts}
+            title={`${user.username}'s thoughts...`}
+          />
+        </div>
+
+        <div className="col-12 col-lg-3 mb-3">
+          <FriendList
+            username={user.username}
+            friendCount={user.friendCount}
+            friends={user.friends}
+          />
+        </div>
       </div>
     </div>
   );
